3fb5a8541aed8459e505c771f610ace5121593c7
motodimago <moto@wizard.st>
2018-04-26 08:12:15 +0900

    Moved function to static.
    Signed-off-by: motodimago <moto@wizard.st>

--- src/helpers/ConfigurationHelper.js
+++ src/helpers/ConfigurationHelper.js
@@ -6,20 +6,21 @@ class ConfigurationHelper {
     static get(name: string, defaultValue = null) {
         return _.get(appConfig, name, defaultValue);
     }
+
+    static getPageTitle(name: string) {
+        const menu = this.get('menu');
+        for (const menuItem of menu) {
+            if (menuItem.name === name) {
+                return menuItem.title;
+            }
+        }
+        return '';
+    }
 }
 
 export function config(name: string, defaultValue = null) {
     return ConfigurationHelper.get(name, defaultValue);
 }
 
-export function getPageTitle(name: string) {
-    const menu = config('menu');
-    for (const menuItem of menu) {
-        if (menuItem.name === name) {
-            return menuItem.title;
-        }
-    }
-    return '';
-}
 
 export default ConfigurationHelper;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,12 +4,12 @@ import Base from "../Base";
 import Screen from "./Screen"
 import Store from "./Store";
 import {Provider, observer} from "mobx-react/native";
-import {getPageTitle} from '../../helpers';
+import ConfigurationHelper from '../../helpers/ConfigurationHelper';
 
 @observer
 class Home extends Base {
     render() {
-        const title = getPageTitle(this.constructor.name);
+        const title = ConfigurationHelper.getPageTitle(this.constructor.name);
         return (
             <Provider ScreenStore={new Store()} {...this.props}>
                 <Screen title={title}/>
